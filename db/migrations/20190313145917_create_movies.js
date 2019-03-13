b57b838bb17d07aff18c990bc4a25a53c202144a
shraddha <shraddha1704@gmail.com>
2019-03-13 16:06:20 -0700

    feat(model): Creating a model to represent data

--- db/migrations/20190313145917_create_movies.js
+++ db/migrations/20190313145917_create_movies.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.up = async (Knex) => {
-        await Knex.schema.createTable('movies', (table) => {
+    await Knex.schema.createTable('movies', (table) => {
         table.increments('id').primary();
         table.text('title').notNullable();
         table.integer('release_year');
