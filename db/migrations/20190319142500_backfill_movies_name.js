8be0d65c0ab9b6bc21a1e637ca5c1a3f4e7b20aa
shraddha <shraddha1704@gmail.com>
2019-03-19 14:42:25 -0700

    remove unused variable

--- db/migrations/20190319142500_backfill_movies_name.js
+++ db/migrations/20190319142500_backfill_movies_name.js
@@ -1,9 +1,9 @@
 'use strict';
 
 exports.up = async (Knex) => {
-  return await Knex.raw('UPDATE movies SET name = title');
+  await Knex.raw('UPDATE movies SET name = title');
 };
 
-exports.down = async (Knex) => {
+exports.down = async (Promise) => {
   return await Promise.resolve();
 };
